81822cc8e2ed9fa0a913b719ba85d25b2cef0716
Asrorjon <turgunboyev7212@gmail.com>
2023-06-06 14:35:43 +0500

    This project for easy bot

--- src/main/java/com/test/easybot/controller/ProductController.java
+++ src/main/java/com/test/easybot/controller/ProductController.java
@@ -12,7 +12,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
